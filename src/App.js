ca1d6d16837a5794f59d3bcd3a922e13958aea7f
shafali03 <shaf03sm@gmail.com>
2020-11-21 19:20:18 +0000

    Created a product  section displaying images, title, description and button of the products

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { GlobalStyle } from './globalStyle'
 import Hero from './components/Hero/Hero'
+import Products from './components/Products/Products'
+import { productData } from './components/Products/data'
 
 const App = () => {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading='Choose your desserts' data={productData} />
     </Router>
   )
 }
